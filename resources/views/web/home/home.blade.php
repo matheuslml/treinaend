3bba1a62dba0b0bb2ab96736d8aebd7b0c8bb596
Matheus de Lima Mendonça <matheusm.uff@gmail.com>
2026-03-05 14:54:06 -0300

    some fixes

--- resources/views/web/home/home.blade.php
+++ resources/views/web/home/home.blade.php
@@ -287,7 +287,7 @@
   {{-- Page js files --}}
           <!-- jQuery (apenas uma vez) --><!-- jQuery (apenas uma vez) -->
 
-            <script src="assets-web/js/site/site.js" src=""></script>
+            <script src="assets-web/js/theme.js" src=""></script>
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
