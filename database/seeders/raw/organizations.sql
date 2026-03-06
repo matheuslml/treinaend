0cec786b906d4068a7383a98d73700a8c2d500fb
Matheus de Lima Mendonça <matheusm.uff@gmail.com>
2026-03-06 10:25:35 -0300

    organization and unir seeders

---
+++ database/seeders/raw/organizations.sql
@@ -0,0 +1,2 @@
+INSERT INTO organizations (title,slug,active,deleted_at,created_at,updated_at) VALUES
+    ('TREINAEND','TREINAEND',1,NULL,NOW(),NULL);
---
+++ database/seeders/raw/units.sql
@@ -0,0 +1,3 @@
+INSERT INTO units (organization_id, name,sigla,phone,email,operation,address,logo,icon,web,deleted_at,created_at,updated_at) VALUES
+    (1,'TREINAEND','TND','22997377972','admin@admin.com','Formando profissionais de alto impacto','ENDEREÇO','logos/teste.jpg','icons/teste.jpg',1,NULL,NOW(),NULL);
+
